897ed739b616f03285eaa941a39ee7ff14662b78
Atul Kumar <you@example.com>
2023-03-20 11:48:20 +0530

    change base url

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ v1(app)
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://whatsapp-web-clone-hdb1.onrender.com",
         methods: ["GET", "POST"],
     }
 })
